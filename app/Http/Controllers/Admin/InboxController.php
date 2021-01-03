c0b60d7a05990dc724de6851b887b3c9873864cf
aanzaenu <44178475+aanzaenu@users.noreply.github.com>
2021-01-03 22:06:59 +0700

    fix inbox model

--- app/Http/Controllers/Admin/InboxController.php
+++ app/Http/Controllers/Admin/InboxController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
-use App\inbox;
+use App\Inbox;
 use App\Setting;
 use Illuminate\Support\Str;
 
