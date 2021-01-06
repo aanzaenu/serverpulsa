72e87300b898abaa6ec216e2393fff566777ddff
aanzaenu <44178475+aanzaenu@users.noreply.github.com>
2021-01-06 11:42:19 +0700

    fix not fillable total for inbox

--- app/Inbox.php
+++ app/Inbox.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Inbox extends Model
 {
     protected $fillable = [
-        'code', 'sender', 'status', 'transaction_id', 'image', 'thumb', 'message', 'tanggal', 'op', 'terminal'
+        'code', 'sender', 'status', 'transaction_id', 'image', 'thumb', 'message', 'tanggal', 'op', 'total', 'terminal'
     ];
 }
