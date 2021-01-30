133971ff7a4a0818b63fbc0151d976a7676ed74b
aanzaenu <44178475+aanzaenu@users.noreply.github.com>
2021-01-31 02:03:26 +0700

    delete tid

--- app/Http/Controllers/InboxController.php
+++ app/Http/Controllers/InboxController.php
@@ -30,12 +30,6 @@ class InboxController extends Controller
                 $lastid = $latest->code;
             }
             $cek = Inbox::where('identifier', $request->get('identifier'))->where('code', $request->get('code'))->first();
-            $terminal = Terminal::where('terminal_id', $request->get('terminal'))->where('pcid', $pcid)->first();
-            $tid = 0;
-            if($terminal)
-            {
-                $tid = $terminal->id;
-            }
             if(!$cek){
                 if(intval($request->get('code')) > $lastid)
                 {
@@ -54,8 +48,7 @@ class InboxController extends Controller
                                 'total' => $request->get('total'),
                                 'op' => 0,
                                 'identifier' => $request->get('identifier'),
-                                'pcid' => $pcid,
-                                'tid' => $tid
+                                'pcid' => $pcid
                             );
                             Inbox::create($array);
                         //}
